18ad800fe63d81367a07d931be65e3cf944cd2a4
giolujan <giolujan123@gmil.com>
2023-02-03 14:17:07 -0500

    ruta absoluta y relativa extension md y lectura de archivo.md

--- cli.js
+++ cli.js
@@ -1,9 +1,38 @@
-const {mdLinks} = require('./index.js');
+const {mdLinks } = require('./index.js');
 
-console.log(mdLinks);
-mdLinks('/noexiste/').then((error)=>{
-  console.log(error)
+/* console.log(mdLinks);
+mdLinks('/noexiste/').then(()=>{
 })
 .catch((error)=>{
    console.log(error)
 })
+
+console.log(mdLinks);
+mdLinks('./index.js').then(()=>{
+})
+.catch((error)=>{
+   console.log(error)
+})
+
+//ruta absoluta
+console.log(mdLinks);
+mdLinks("D:/coderHouse Programacion web/class 01/css/style.css").then(()=>{
+})
+.catch((error)=>{
+   console.log(error)
+}); 
+
+//ruta absoluta
+console.log(mdLinks);
+mdLinks("C:/Users/Usuario/Desktop/Laboratoria/DEV001-card-validation/README.md").then(()=>{
+})
+.catch((error)=>{
+   console.log(error)
+}); */
+
+console.log(mdLinks);
+mdLinks("C:/Users/Usuario/Desktop/Laboratoria/DEV001-md-links/testReadme.md").then(()=>{
+})
+.catch((error)=>{
+   console.log(error)
+});
--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 const fs = require("fs");
+const path = require("path")
 
-const mdLinks = (path, options) => {
+const mdLinks = (inputPath, options) => {
   return new Promise((resolve, reject) => {
-    //identificar si la ruta existe
-    if(fs.existsSync(path)){
-      // revisar y convertir a una ruta absoluta
-      // probar si es la ruta absoluta es un archivo o directorio
-      // si es un directorio devolver un arreglo de md 
-    }else {
-    //si la ruta no existe se rechaza la promesa
-    reject('la ruta no existe');
+    //comprueba si una ruta existe o no
+    if (fs.existsSync(inputPath)) {
+
+      if (path.isAbsolute(inputPath)) {
+        //Comprueba si un archivo es una ruta absoluta o no.
+        //reject("la  ruta es absoluta")
+        const extension = path.extname(inputPath)
+        //Devuelve la extensión de la ruta. 
+        if ((extension === '.md')) {
+          //reject("la  ruta es .md")
+          fs.readFile( inputPath , 'utf-8', (err, data) => {
+            if (err) {
+              console.log('error: ', err);
+            } else {
+              //console.log(data);
+              fs.readdir(inputPath , (err, files) => {
+                if (err)
+                  console.log(err);
+                else {
+                  console.log(file);/*
+                  files.forEach(file => {
+                    console.log(file);
+                })*/
+                }
+              })
+            }
+          });
+        } else {
+          reject(path.extname(inputPath))
+        }
+      } else {
+        //permite obtener la ruta absoluta 
+        const relativePath = path.resolve(inputPath)
+        //Devuelve el nombre de directorio de una ruta.
+        const path1 = path.dirname(relativePath)
+        //Devuelve la última parte de una ruta. 
+        const path2 = path.basename(relativePath)
+        //Junta todos los argumentos y normaliza la ruta resultante.
+        reject(path.join(path1, path2))
+      }
+
+      // leer el archivo md
+      // sacar los links del contenido con las propiedades href text file 
+      // si opcion validate es true se valida con status ok
+      //  para resolver la promesa devolver un arreglo de links
+    } else {
+      //si la ruta no existe se rechaza la promesa
+      reject('la ruta no existe');
     }
   });
 };
 
+/*
+const relativePath = (mdLinks) => {
+  return new Promise((resolve, reject) => {
+    reject ( path.dirname(inputPath) )
+  })
+}*/
+
 module.exports = {
   mdLinks
- };
+};
